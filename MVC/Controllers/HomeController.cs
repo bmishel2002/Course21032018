2ecf19b5ce305dc150cb91e028b9e92f76737fdd
Бондаренко Михаил <bmishel2002@gmail.com>
2018-03-25 20:13:38 +0300

    - Добавил MVC проект. - Спеку и презентацию

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using Newtonsoft.Json;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<UserModel> users = new List<UserModel>();
+
+            using (var httpClinet = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage res = httpClinet.GetAsync("http://localhost:5000/api/users").Result;
+                    res.EnsureSuccessStatusCode();
+                    users = JsonConvert.DeserializeObject<List<UserModel>>(res.Content.ReadAsStringAsync().Result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+
+            }
+            
+            return View(users);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ MVC/Models/UserModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MVC.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+
+        public string  Name { get; set; }}
+}
